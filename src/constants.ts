f92c32423131cb6f835e2f398ba8920accea57f7
Anthony C. <anthony.chan.2509@gmail.com>
2025-12-16 22:16:10 +0900

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,8 @@ export const LOCALE_CACHE_KEY = 'cdnLocaleCache_v1'
 /**
  * List of CSS selectors to exclude from translation.
  * Any text node inside an element matching these selectors (or the element itself) will be skipped.
+ * Test site - look for the text "EXCLUDE-THIS" (testing string: notranslate):
+ * https://preview.webflow.com/preview/notranslate?preview=1db9e911959b56d89fe51bfa4a4962bb
  */
 export const EXCLUDED_SELECTORS = [
     // Examples:
@@ -13,17 +15,17 @@ export const EXCLUDED_SELECTORS = [
     // '.top-bar',          // Class
     // '[data-pop="wow"]',  // Attribute
     // 'nav.top',           // Tag + Class
-    // Parts in Webflow, especially in the Designer, that should not be translated:
     // Webflow Pages
-    'div.bem-SearchResultPreview',
     // Webflow Designer
+    'div.bem-SearchResultPreview', // SEO Preview
     '[data-automation-id="page-list-row-wrapper"] div.bem-List_Cell',
     '[data-palette="CMSListItem"]',
     '[data-palette="CMSItemsListPanelTitle"]',
     '[data-palette="VirtualizedSearchableListGroup"] [data-sc="RowItemLabel"]',
     '[data-automation-id="style-rule-token-text"]',
-    '[data-automation-id="symbol-panel-styles-name"]',  // Style Panel > Styles names
-    '[data-automation-id="style-rule-token-all-styles-tooltip-text"]',  // Style Selector > Style names
+    '[data-automation-id="symbol-panel-styles-name"]', // Style Panel > Styles names
+    '[data-automation-id="style-rule-token-all-styles-tooltip-text"]', // Style Selector > Style names
     '[data-automation-id="components-group-Templates"] span[data-text="true"]',
+    '[data-automation-id="default-collection-row"]', // Variable Group names
     '.CodeMirror-scroll'
 ];
