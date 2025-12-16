4858fa6864d7368024ba0c139476378a5f108586
Anthony C. <anthony.chan.2509@gmail.com>
2025-12-16 19:47:59 +0900

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -9,15 +9,21 @@ export const LOCALE_CACHE_KEY = 'cdnLocaleCache_v1'
  */
 export const EXCLUDED_SELECTORS = [
     // Examples:
-    // '#apple-pie',        // ID example
-    // '.top-bar',          // Class example
-    // '[data-pop="wow"]',  // Attribute example
-    // 'nav.top',           // Tag + Class example
-    // There are parts in Webflow, especially in the Designer, that should not be translated.
-    // Default selectors here:
+    // '#apple-pie',        // ID
+    // '.top-bar',          // Class
+    // '[data-pop="wow"]',  // Attribute
+    // 'nav.top',           // Tag + Class
+    // Parts in Webflow, especially in the Designer, that should not be translated:
+    // Webflow Pages
     'div.bem-SearchResultPreview',
+    // Webflow Designer
     '[data-automation-id="page-list-row-wrapper"] div.bem-List_Cell',
     '[data-palette="CMSListItem"]',
     '[data-palette="CMSItemsListPanelTitle"]',
-    '[data-palette="VirtualizedSearchableListGroup"] [data-sc="RowItemLabel"]'
+    '[data-palette="VirtualizedSearchableListGroup"] [data-sc="RowItemLabel"]',
+    '[data-automation-id="style-rule-token-text"]',
+    '[data-automation-id="symbol-panel-styles-name"]',  // Style Panel > Styles names
+    '[data-automation-id="style-rule-token-all-styles-tooltip-text"]',  // Style Selector > Style names
+    '[data-automation-id="components-group-Templates"] span[data-text="true"]',
+    '.CodeMirror-scroll'
 ];
